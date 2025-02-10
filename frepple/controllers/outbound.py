ff4e64b25f6f9f3558181726838d7e50e321f195
Hicham Lahlou <hlahlou@frepple.com>
2025-02-10 22:00:28 +0100

    filter quote sos

--- frepple/controllers/outbound.py
+++ frepple/controllers/outbound.py
@@ -2029,15 +2029,22 @@ class exporter(object):
         """
         # Get all sales order lines
         search = (
-            [("product_id", "!=", False)]
+            [
+                "&",
+                ("product_id", "!=", False),
+                ("order_id.state", "not in", ["draft", "sent", "cancel"]),
+            ]
             if self.delta >= 999
             else [
+                "&",
+                "&",
                 ("product_id", "!=", False),
                 (
                     "write_date",
                     ">=",
                     datetime.now() - timedelta(days=self.delta),
                 ),
+                ("order_id.state", "not in", ["draft", "sent", "cancel"]),
             ]
         )
         so_line = self.generator.getData(

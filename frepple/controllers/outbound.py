4dfd9b8dbaac3a3e34a2ad96aa3e57e26123bb65
Hicham Lahlou <hlahlou@frepple.com>
2025-05-22 09:57:17 +0200

    consider negative stock at postprd

--- frepple/controllers/outbound.py
+++ frepple/controllers/outbound.py
@@ -3337,9 +3337,10 @@ class exporter(object):
                 stock_lot.name as lot_name,
                 stock_lot.expiration_date
                 FROM stock_quant
+                INNER JOIN stock_location ON stock_quant.location_id = stock_location.id
                 left outer join stock_lot on stock_quant.lot_id = stock_lot.id
                 and stock_lot.product_id = stock_quant.product_id
-                WHERE quantity > 0
+                WHERE (stock_quant.quantity > 0 or stock_location.name = 'POSTPRD')
                 GROUP BY stock_quant.product_id,
                 stock_quant.location_id,
                 stock_lot.name,
@@ -3427,7 +3428,7 @@ class exporter(object):
                 "SELECT product_id, stock_quant.location_id, sum(quantity), sum(reserved_quantity) "
                 "FROM stock_quant "
                 "INNER JOIN stock_location ON stock_quant.location_id = stock_location.id "
-                "WHERE quantity > 0 "
+                "WHERE (quantity > 0 or stock_location.name = 'POSTPRD') "
                 "AND stock_location.scrap_location is distinct from true "
                 "AND stock_location.return_location is distinct from true "
                 "AND stock_location.usage = 'internal' "

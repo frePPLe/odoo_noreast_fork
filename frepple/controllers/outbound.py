5e74364a4e68f7987da7c317d6bb9aaacff0183f
Hicham Lahlou <hlahlou@frepple.com>
2025-06-18 22:51:28 +0200

    double quotes not set at the right place

--- frepple/controllers/outbound.py
+++ frepple/controllers/outbound.py
@@ -1954,7 +1954,7 @@ class exporter(object):
                                     )
                                 )
 
-                            yield "<suboperation>" '<operation name=%s %spriority="%s" %s="%s" xsi:type="%s">\n' "<location name=%s/>\n" '<doubleproperty name="employee_ratio" value="%s"/>' '<stringproperty name="flowtype" value="%s"/>' '<loads><load quantity="%f" search=%s><resource name=%s/>%s</load>%s</loads>\n' % (
+                            yield "<suboperation>" '<operation name=%s %spriority="%s" %s="%s" xsi:type="%s">\n' "<location name=%s/>\n" '<doubleproperty name="employee_ratio" value="%s"/>' '<stringproperty name="flowtype" value=%s/>' '<loads><load quantity="%f" search=%s><resource name=%s/>%s</load>%s</loads>\n' % (
                                 quoteattr(name),
                                 (
                                     ("description=%s " % quoteattr(i["code"]))
@@ -2900,7 +2900,7 @@ class exporter(object):
                             units="minutes",
                         ),
                         quoteattr(location),
-                        ('<stringproperty name="flowtype" value=%s/>' % (wo.flowtype,)) if wo.flowtype else "",
+                        ('<stringproperty name="flowtype" value="%s"/>' % (wo.flowtype,)) if wo.flowtype else "",
                     )
                     idx += 10
                     # dictionary needed as BOM in Odoo might have multiple lines with the same product

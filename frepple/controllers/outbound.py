9cd9d024ccf279be6906760b945430dc5e816db3
hichamlahlou <hlahlou@frepple.com>
2026-01-30 16:30:12 +0100

    sort using the id of the workorder

--- frepple/controllers/outbound.py
+++ frepple/controllers/outbound.py
@@ -24,6 +24,7 @@
 
 import json
 import logging
+from operator import attrgetter
 import pytz
 import xmlrpc.client
 from xml.sax.saxutils import quoteattr
@@ -2816,7 +2817,7 @@ class exporter(object):
 
             # Collect work order info
             if self.manage_work_orders:
-                wo_list = sorted(i.workorder_ids or [])
+                wo_list = sorted(i.workorder_ids or [], key=attrgetter("id"))
             else:
                 wo_list = []
 
